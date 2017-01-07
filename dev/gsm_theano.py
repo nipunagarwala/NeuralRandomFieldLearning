32063ceb8b2aae97923d75853556545e3614f111
mhw32 <me@mikewuis.me>
2017-01-07 15:12:00 -0800

    explicit initialization specifications for dense_layers Bug #1 FOUND: forgot to separate eval_fun and training_fun

--- dev/gsm_theano.py
+++ dev/gsm_theano.py
@@ -18,29 +18,29 @@ n_in = n_out = n_chan*n_dim*n_dim  # input/output size
 x = T.matrix(dtype=theano.config.floatX)
 tau = theano.shared(
     np.float32(1.0), name='temperature',
-    allow_downcast=True,
+    allow_downcast=True, borrow=False,
 )
 lr = theano.shared(
     np.float32(0.001), name='learning_rate',
-    allow_downcast=True,
+    allow_downcast=True, borrow=False,
 )
 
 # encoder design
 net = InputLayer((None, n_in), x)
-net = DenseLayer(net, 512, nonlinearity=T.nnet.relu)
-net = DenseLayer(net, 256, nonlinearity=T.nnet.relu)
+net = DenseLayer(net, 512, nonlinearity=T.nnet.relu, W=lasagne.init.GlorotUniform())
+net = DenseLayer(net, 256, nonlinearity=T.nnet.relu, W=lasagne.init.GlorotUniform())
 # bottleneck design
-logits_y = DenseLayer(net, n_cat*n_class, nonlinearity=None)
+logits_y = DenseLayer(net, n_cat*n_class, nonlinearity=None, W=lasagne.init.GlorotUniform())
 logits_y = reshape(logits_y, (-1, n_class))
 y = GumbelSoftmaxSampleLayer(logits_y, tau)
 y = reshape(y, (-1, n_cat, n_class))
 # decoder design
-net = DenseLayer(flatten(y), 256, nonlinearity=T.nnet.relu)
-net = DenseLayer(net, 512, nonlinearity=T.nnet.relu)
-logits_x = DenseLayer(net, n_out, nonlinearity=None)
+net = DenseLayer(flatten(y), 256, nonlinearity=T.nnet.relu, W=lasagne.init.GlorotUniform())
+net = DenseLayer(net, 512, nonlinearity=T.nnet.relu, W=lasagne.init.GlorotUniform())
+logits_x = DenseLayer(net, n_out, nonlinearity=None, W=lasagne.init.GlorotUniform())
 
 # define the loss
-_logits_y, _logits_x = lasagne.layers.get_output([logits_y, logits_x])
+_logits_y, _logits_x = lasagne.layers.get_output([logits_y, logits_x], deterministic=False)
 q_y = T.nnet.softmax(_logits_y)
 log_q_y = T.log(q_y + 1e-20)
 log_p_x = log_bernoulli(x, _logits_x)
@@ -51,9 +51,10 @@ elbo = T.sum(log_p_x, 1) - KL
 loss = T.mean(-elbo)
 
 # network compilation
-params = get_all_params(logits_x)
+params = get_all_params(logits_x, trainable=True)
 updates = lasagne.updates.adam(loss, params, learning_rate=lr)
 train_op = theano.function([x], loss, updates=updates)
+eval_op = theano.function([x], loss)
 
 # training
 BATCH_SIZE = 100
@@ -67,6 +68,8 @@ MIN_TEMP = 0.5
 from tensorflow.examples.tutorials.mnist import input_data
 data = input_data.read_data_sets('/tmp/', one_hot=True).train
 
+tau.set_value(np_temp, borrow=False)
+lr.set_value(np_lr, borrow=False)
 for i in range(1, NUM_ITERS):
     sample, _ = data.next_batch(BATCH_SIZE)
     np_loss = train_op(sample)
@@ -75,9 +78,10 @@ for i in range(1, NUM_ITERS):
     if i % 1000 == 1:
         np_temp = np.maximum(tau0*np.exp(-ANNEAL_RATE*i), MIN_TEMP)
         np_lr *= 0.9
-        tau.set_value(np_temp)
-        lr.set_value(np_lr)
+        tau.set_value(np_temp, borrow=False)
+        lr.set_value(np_lr, borrow=False)
 
     # print log statement
     if i % 5000 == 1:
-        print('Step %d, ELBO: %0.3f' % (i, -np_loss))
+        _np_loss = eval_op(sample)
+        print('Step %d, ELBO: %0.3f' % (i, -_np_loss))
