73ea561a236c392ec4569bb3492bf3dc732c0521
mhw32 <me@mikewuis.me>
2017-01-14 11:32:03 -0800

    cleaning up unnecessary models + added GaussianMultiSampleLayer and RepeatLayer

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 from softmax import Softmax
 from mlp import MLP
-from cnn import CNN
 from vae import VAE
 from sbn import SBN
 from sbn_gsm import SBN_GSM
@@ -10,8 +9,3 @@ from adgm_gsm import ADGM_GSM
 from vae_reinforce import VAE_REINFORCE
 from gsm import GSM
 from rbm import RBM
-
-try:
-  from resnet import Resnet
-except:
-  print 'WARNING: Could not import Resnet; you might need to upgrade Lasagne.'
