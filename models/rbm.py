87264adf7bcb94631ed8c3e7a687630340d784fc
mhw32 <me@mikewuis.me>
2017-01-16 17:45:32 -0800

    Migrating rbm to be purely inherited from Model

--- models/rbm.py
+++ models/rbm.py
@@ -11,33 +11,47 @@ from theano.tensor.shared_randomstreams import RandomStreams
 from model import Model
 from helpers import *
 
+theano.config.optmizer = 'None'
 
 class RBM(Model):
-    '''Restricted Boltzmann Machine
+    """Restricted Boltzmann Machine
     RBM code adapted from http://deeplearning.net/tutorial/rbm.html
-    '''
+
+    Epoch 15 of 15 took 168.265s (2500 minibatches)
+        training loss/acc:		-62.755739271	None
+
+    Training Params
+    ---------------
+    batch_size: 20
+    learning_rate: 0.1
+    """
     def __init__(
         self, n_dim, n_out, n_chan=1, n_superbatch=12800, opt_alg='adam',
         opt_params={'lr': 1e-3, 'b1': 0.9, 'b2': 0.99}
     ):
-        '''RBM constructor.
+        """RBM constructor.
         Defines the parameters of the model along with
         basic operations for inferring hidden from visible (and vice-versa),
         as well as for performing CD updates.
-        '''
+        """
         self.numpy_rng = np.random.RandomState(1234)
         self.theano_rng = RandomStreams(self.numpy_rng.randint(2 ** 30))
-        self.create_model(n_dim, n_out, n_chan)
 
+        # extract training params
         lr = opt_params.get('lr')
         n_batch = opt_params.get('nb')
 
         # create shared objects for x and y
         train_set_x = theano.shared(
             np.empty(
-                (n_superbatch, n_chan*n_dim*n_dim),
+                (n_superbatch, n_chan, n_dim, n_dim),
                 dtype=theano.config.floatX
             ), borrow=False,
+        )
+        val_set_x = theano.shared(np.empty(
+            (n_superbatch, n_chan, n_dim, n_dim),
+            dtype=theano.config.floatX),
+            borrow=False,
         )
 
         train_set_y = theano.shared(
@@ -46,10 +60,25 @@ class RBM(Model):
                 dtype=theano.config.floatX
             ), borrow=False,
         )
+        val_set_y = theano.shared(
+            np.empty(
+                (n_superbatch,),
+                dtype=theano.config.floatX
+            ), borrow=False,
+        )
+        train_set_y_int = T.cast(train_set_y, 'int32')
+        val_set_y_int = T.cast(val_set_y, 'int32')
 
         # allocate symbolic variables for the data
-        index = T.lscalar()    # index to a [mini]batch
-        x = T.matrix('x')
+        X = T.tensor4(dtype=theano.config.floatX)
+        S = T.tensor3(dtype=theano.config.floatX)
+        Y = T.ivector()
+        idx1, idx2 = T.lscalar(), T.lscalar()
+        alpha = T.scalar(dtype=theano.config.floatX)  # adjustable learning rate
+        self.inputs = (X, Y, idx1, idx2, S)
+
+        # create model
+        self.network = self.create_model(n_dim, n_out, n_chan)
 
         # initialize storage for the persistent chain (state = hidden
         # layer of chain)
@@ -61,21 +90,38 @@ class RBM(Model):
         )
 
         # get the cost and the gradient corresponding to one step of CD-15
-        cost, updates = self.get_cost_updates(x, lr=lr, persistent=persistent_chain)
+        cost, acc, updates = self.get_cost_updates(
+            X, alpha, lr=lr, persistent=persistent_chain,
+        )
+        self.objectives = (cost, acc)
 
         self.train = theano.function(
-            [index],
-            cost,
+            [idx1, idx2, alpha],
+            [cost, acc],
             updates=updates,
-            givens={ x: train_set_x[index * n_batch: (index + 1) * n_batch] },
-            name='train_rbm',
+            givens={X : train_set_x[idx1:idx2], Y : train_set_y_int[idx1:idx2]},
             on_unused_input='warn',
         )
 
         self.n_batch = n_batch
-        self.loss = theano.function([x], cost,  on_unused_input='warn')
+        self.loss = theano.function([X, Y], [cost, acc], on_unused_input='warn')
+
+        # save config
+        self.n_dim = n_dim
+        self.n_out = n_out
+        self.n_superbatch = n_superbatch
+        self.alg = opt_alg
+
+        # save data variables
         self.train_set_x = train_set_x
         self.train_set_y = train_set_y
+        self.val_set_x = val_set_x
+        self.val_set_y = val_set_y
+        self.data_loaded = False
+
+        # save neural network
+        self.params = self.get_params()
+        self.metrics = (cost, acc)
 
     def create_model(self, n_dim, n_out, n_chan=1):
         n_visible = n_chan*n_dim*n_dim  # size of visible layer
@@ -131,14 +177,14 @@ class RBM(Model):
         return None
 
     def free_energy(self, v_sample):
-        '''Function to compute the free energy'''
+        """Function to compute the free energy"""
         wx_b = T.dot(v_sample, self.W) + self.hbias
         vbias_term = T.dot(v_sample, self.vbias)
         hidden_term = T.sum(T.log(1 + T.exp(wx_b)), axis=1)
         return -hidden_term - vbias_term
 
     def propup(self, vis):
-        '''This function propagates the visible units activation upwards to
+        """This function propagates the visible units activation upwards to
         the hidden units
 
         Note that we return also the pre-sigmoid activation of the
@@ -146,12 +192,12 @@ class RBM(Model):
         optimizations, this symbolic variable will be needed to write
         down a more stable computational graph (see details in the
         reconstruction cost function)
-        '''
+        """
         pre_sigmoid_activation = T.dot(vis, self.W) + self.hbias
         return [pre_sigmoid_activation, T.nnet.sigmoid(pre_sigmoid_activation)]
 
     def sample_h_given_v(self, v0_sample):
-        '''This function infers state of hidden units given visible units'''
+        """This function infers state of hidden units given visible units"""
         # compute the activation of the hidden units given a sample of
         # the visibles
         pre_sigmoid_h1, h1_mean = self.propup(v0_sample)
@@ -167,7 +213,7 @@ class RBM(Model):
         return [pre_sigmoid_h1, h1_mean, h1_sample]
 
     def propdown(self, hid):
-        '''This function propagates the hidden units activation downwards to
+        """This function propagates the hidden units activation downwards to
         the visible units
 
         Note that we return also the pre_sigmoid_activation of the
@@ -175,12 +221,12 @@ class RBM(Model):
         optimizations, this symbolic variable will be needed to write
         down a more stable computational graph (see details in the
         reconstruction cost function)
-        '''
+        """
         pre_sigmoid_activation = T.dot(hid, self.W.T) + self.vbias
         return [pre_sigmoid_activation, T.nnet.sigmoid(pre_sigmoid_activation)]
 
     def sample_v_given_h(self, h0_sample):
-        '''This function infers state of visible units given hidden units'''
+        """This function infers state of visible units given hidden units"""
         # compute the activation of the visible given the hidden sample
         pre_sigmoid_v1, v1_mean = self.propdown(h0_sample)
         # get a sample of the visible given their activation
@@ -195,18 +241,18 @@ class RBM(Model):
         return [pre_sigmoid_v1, v1_mean, v1_sample]
 
     def gibbs_hvh(self, h0_sample):
-        '''This function implements one step of Gibbs sampling,
+        """This function implements one step of Gibbs sampling,
         starting from the hidden state
-        '''
+        """
         pre_sigmoid_v1, v1_mean, v1_sample = self.sample_v_given_h(h0_sample)
         pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v1_sample)
         return [pre_sigmoid_v1, v1_mean, v1_sample,
                 pre_sigmoid_h1, h1_mean, h1_sample]
 
     def gibbs_vhv(self, v0_sample):
-        '''This function implements one step of Gibbs sampling,
+        """This function implements one step of Gibbs sampling,
         starting from the visible state
-        '''
+        """
         pre_sigmoid_h1, h1_mean, h1_sample = self.sample_h_given_v(v0_sample)
         pre_sigmoid_v1, v1_mean, v1_sample = self.sample_v_given_h(h1_sample)
         return [pre_sigmoid_h1, h1_mean, h1_sample,
@@ -215,8 +261,8 @@ class RBM(Model):
     def get_params(self):
         return self.params
 
-    def get_cost_updates(self, X, lr=0.1, persistent=None):
-        '''Returns the updates dictionary.
+    def get_cost_updates(self, X, alpha, lr=0.1, persistent=None):
+        """Returns the updates dictionary.
         The dictionary contains the update rules
         for weights and biases but also an update of the shared variable used to
         store the persistent chain, if one is used.
@@ -230,10 +276,11 @@ class RBM(Model):
         dictionary contains the update rules for weights and biases but
         also an update of the shared variable used to store the persistent
         chain, if one is used.
-        '''
+        """
+        x = X.flatten(2)
 
         # compute positive phase
-        pre_sigmoid_ph, ph_mean, ph_sample = self.sample_h_given_v(X)
+        pre_sigmoid_ph, ph_mean, ph_sample = self.sample_h_given_v(x)
 
         # decide how to initialize persistent chain:
         # for CD, we use the newly generate hidden sample
@@ -270,10 +317,10 @@ class RBM(Model):
         # note that we only need the sample at the end of the chain
         chain_end = nv_samples[-1]
 
-        cost = T.mean(self.free_energy(X)) - T.mean(self.free_energy(chain_end))
+        cost = T.mean(self.free_energy(x)) - T.mean(self.free_energy(chain_end))
         # We must not compute the gradient through the gibbs sampling
         gparams = T.grad(cost, self.params, consider_constant=[chain_end])
-        # gparams = [grad * alpha for grad in gparams]
+        gparams = [grad * alpha for grad in gparams]
 
         # constructs the update dictionary
         for gparam, param in zip(gparams, self.params):
@@ -284,16 +331,17 @@ class RBM(Model):
             # Note that this works only if persistent is a shared variable
             updates[persistent] = nh_samples[-1]
             # pseudo-likelihood is a better proxy for PCD
-            monitoring_cost = self.get_pseudo_likelihood_cost(X, updates)
+            monitoring_cost = self.get_pseudo_likelihood_cost(x, updates)
         else:
             # reconstruction cross-entropy is a better proxy for CD
             monitoring_cost = self.get_reconstruction_cost(
-                X, updates, pre_sigmoid_nvs[-1])
+                x, updates, pre_sigmoid_nvs[-1])
 
-        return monitoring_cost, updates
+        # TODO fill in real accuracies
+        return monitoring_cost, monitoring_cost, updates
 
     def get_pseudo_likelihood_cost(self, X, updates):
-        '''Stochastic approximation to the pseudo-likelihood'''
+        """Stochastic approximation to the pseudo-likelihood"""
         # index of bit i in expression p(x_i | x_{\i})
         bit_i_idx = theano.shared(value=0, name='bit_i_idx')
 
@@ -320,7 +368,7 @@ class RBM(Model):
         return cost
 
     def get_reconstruction_cost(self, X, updates, pre_sigmoid_nv):
-        '''
+        """
         Approximation to the reconstruction error
 
         Note that this function requires the pre-sigmoid activation as
@@ -347,7 +395,7 @@ class RBM(Model):
         is to get also the pre-sigmoid activation as an output of
         scan, and apply both the log and sigmoid outside scan such
         that Theano can catch and optimize the expression.
-        '''
+        """
 
         cross_entropy = T.mean(
             T.sum(
@@ -360,35 +408,51 @@ class RBM(Model):
         return cross_entropy
 
     def fit(self, X_train, Y_train, X_val, Y_val, n_epoch=10, n_batch=100, logname='run'):
-        ''' Train the model'''
-        X_train = X_train.reshape(-1, np.prod(X_train.shape[1:]))
-        X_val = X_val.reshape(-1, np.prod(X_val.shape[1:]))
+        """Train the model"""
 
-        start_time = timeit.default_timer()
+        alpha = 1.0 # learning rate, which can be adjusted later
+        n_data = len(X_train)
+        n_superbatch = self.n_superbatch
 
-        # compute number of minibatches for training, validation and testing
-        n_train_batches = X_train.shape[0] // n_batch
-        self.load_data(X_train, Y_train, dest='train')
-
-        # go through training epochs
         for epoch in range(n_epoch):
-            # go through the training set
-            mean_cost = []
-            for batch_index in range(n_train_batches):
-                mean_cost += [self.train(batch_index)]
+            # In each epoch, we do a full pass over the training data:
+            train_batches, train_err, train_acc = 0, 0, 0
+            start_time = time.time()
+
+            # iterate over superbatches to save time on GPU memory transfer
+            for X_sb, Y_sb in self.iterate_superbatches(
+                X_train, Y_train, n_superbatch,
+                datatype='train', shuffle=True,
+            ):
+                for idx1, idx2 in iterate_minibatch_idx(len(X_sb), n_batch):
+                    err, acc = self.train(idx1, idx2, alpha)
+                    # collect metrics
+                    train_batches += 1
+                    train_err += err
+                    train_acc += acc
+
+                    if train_batches % 100 == 0:
+                        n_total = epoch * n_data + n_batch * train_batches
+                        metrics = [n_total, train_err / train_batches, train_acc / train_batches]
+                        log_metrics(logname, metrics)
 
             print "Epoch {} of {} took {:.3f}s ({} minibatches)".format(
-                epoch + 1, n_epoch, time.time() - start_time, n_train_batches)
-            print "  training loss/acc:\t\t{:.6f}\t{}".format(np.mean(mean_cost), None)
+                epoch + 1, n_epoch, time.time() - start_time, train_batches)
 
-        end_time = timeit.default_timer()
-        pretraining_time = (end_time - start_time)
-        print ('Training took %f minutes' % (pretraining_time / 60.))
+            # make a full pass over the training data and record metrics:
+            train_err, train_acc = evaluate(self.loss, X_train, Y_train, batchsize=1000)
+            val_err, val_acc = evaluate(self.loss, X_val, Y_val, batchsize=1000)
+
+            print "  training loss/acc:\t\t{:.6f}\t{:.6f}".format(train_err, train_acc)
+            print "  validation loss/acc:\t\t{:.6f}\t{:.6f}".format(val_err, val_acc)
+
+            metrics = [ epoch, train_err, train_acc, val_err, val_acc ]
+            log_metrics(logname + '.val', metrics)
 
     def load_params(self, params):
-        ''' Load a given set of parameters '''
+        """Load a given set of parameters"""
         self.params = params
 
     def dump_params(self):
-        ''' Dump a given set of parameters '''
+        """Dump a given set of parameters"""
         return self.params
